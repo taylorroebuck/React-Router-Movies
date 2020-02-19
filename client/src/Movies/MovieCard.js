29bbc388454f67c8ae2b87657fbdff158a6ea63d
Taylor Roebuck <taylornroebuck@gmail.com>
2020-02-18 18:17:58 -0700

    achieved MVP

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const MovieCard = props => {
+  console.log("Movie", props)
   return;
 };
 
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+const SavedList = props => {
+  const history = useHistory();
+
+  function routeToHome() {
+    history.push("/");
+  }
+  
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <div className="home-button" onClick={routeToHome} >Home</div>
+    </div>
+  );
+}
+
+
 
 export default SavedList;
